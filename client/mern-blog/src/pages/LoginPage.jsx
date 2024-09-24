af784de26636bc420aa8ad930dd639d22480c2b1
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-09-24 13:05:20 -0300

    Finish UI

---
+++ client/mern-blog/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return (
+      <form className="form-container login">
+        <h2>Fazer Login</h2>
+        <input type="text" placeholder="Nome de usuário" />
+        <input type="password" placeholder="Sua senha" />
+        <button>Entrar</button>
+      </form>
+    );
+  };
+  
---
+++ client/mern-blog/src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+export default function RegisterPage() {
+    return (
+      <form className="form-container register">
+        <h2>Cadastrar-se</h2>
+        <input type="text" placeholder="Nome de usuário" />
+        <input type="password" placeholder="Sua senha" />
+        <button>Cadastrar</button>
+      </form>
+    );
+  };
+  
