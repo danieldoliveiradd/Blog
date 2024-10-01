f9315f55c5d03333e25f376c5120c268b62ccf19
Daniel Oliveira <danieldoliveiraddjob@gmail.com>
2024-10-01 02:47:04 -0300

    Create a new post system

--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const port = 4000;
-const jwt = require('jsonwebtoken');
-const secret = 'asasfas6fqwfwqfqw6';
+const secret = "asasfas6fqwfwqfqw6";
 
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use(cookieParser());
 
 async function startServer() {
   try {
@@ -21,10 +24,9 @@ async function startServer() {
     app.post("/register", async (req, res) => {
       const { username, password } = req.body;
 
-      // Verifica se o usuário já existe
       const existingUser = await User.findOne({ username });
       if (existingUser) {
-        return res.status(400).json({ message: 'Usuário já existe' });
+        return res.status(400).json({ message: "Usuário já existe" });
       }
 
       const hashedPassword = bcrypt.hashSync(password, 10);
@@ -39,23 +41,39 @@ async function startServer() {
       const { username, password } = req.body;
       const userDoc = await User.findOne({ username });
       if (!userDoc) {
-        return res.status(400).json({ message: 'Usuário não encontrado' });
+        return res.status(400).json({ message: "Usuário não encontrado" });
       }
 
       const passOk = bcrypt.compareSync(password, userDoc.password);
       if (!passOk) {
-        return res.status(400).json({ message: 'Senha incorreta' });
+        return res.status(400).json({ message: "Senha incorreta" });
       }
 
-      // Geração do token JWT
       jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
         if (err) {
-          res.cookie('token', token).json('ok');
-          return res.status(500).json({ message: 'Erro ao gerar token' });
+          return res.status(500).json({ message: "Erro ao gerar token" });
         }
-        res.status(200).json({ token }); // Envia o token ao cliente
+        
+        res.cookie("token", token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+        res.json("ok");
       });
     });
+
+    app.get("/profile", (req, res) => {
+      const { token } = req.cookies;
+      if (!token) {
+        return res.status(401).json({ message: "Token não fornecido" });
+      }
+
+      jwt.verify(token, secret, {}, (err, info) => {
+        if (err) return res.status(401).json({ message: "Token inválido" });
+        res.json(info);
+      });
+    });
+
+    app.post('/logout', (req, res) => {
+      res.cookie('token', '').json("");
+    })
 
     app.listen(port, () => {
       console.log(`Servidor iniciado na porta: ${port}`);
--- client/mern-blog/src/Header.jsx
+++ client/mern-blog/src/Header.jsx
@@ -1,15 +1,45 @@
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Header () {
-    return(
-        <header>
-        <Link to={"/"} className="logo">
-          My Blog
-        </Link>
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </nav>
-      </header>
-    )
+export default function Header() {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5173/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:5173/logout', {
+      credentials: 'include',
+      method: 'POST'
+    })
+    setUsername(null);
+  }
+
+  return (
+    <header>
+      <Link to={"/"} className="logo">
+        My Blog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
